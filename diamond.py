fd17f3b3d3e26a129cdcf79cc94fa6bfb7961004
sanjayaajo <50911421+sanjayaajo@users.noreply.github.com>
2019-08-02 18:25:18 +0700

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik('\033[39m\033[33;1msebelum mulai subcribe dulu chanell youtube sanjaya dtm\033[39m\033[33;1m')
+mengetik('\033[36;1mkalau enggak mandul 7 turunan mampus kau hayo cepet subcribe\033[36;1m')
+mengetik('kalau sudah subcribe thankss')
+print ('\033[31;1m\033[0;36m==============================\033[0;36m')
+print ('\033[31;1m\033[0;36m|  WELCOME TO TOP UP DIAMOND |\033[0;36m')
+print ('\033[39m\033[31m|        PERINGATAN!!!       |\033[39m')
+print ('\033[39m\033[33;1m|  JANGAN SERING ISI DIAMOND |\033[31;1\033[33;1m')
+print ('\033[31;1\033[33;1m|     DARI SINI BISA BAHAYA  |\033[31;1\033[33;1m')
+print ('\033[31;1\033[32;1m|      1 HARI CUKUP SEKALI   |\033[31;1m\033[32;1m')
+print ('\033[31;1m\033[0;36m|   THANKS BYE BYE           |\033[31;1m\033[0;36m')
+print ('\033[31;1m\033[0;36m==============================\033[31;1m\033[0;36m')
+print ('\033[39m>>\033[31mMasukan ID Free Fire\033[39m<<')
+print ('\033[39m[\033[31mSalah ID Bisa Masuk Tempat Orang\033[39m]')
+salam = "Selamat Pagi Agan Agan semua"
+
+idt = raw_input("\033[39m[\033[31m*\033[39m] ID Free Fire   : ")
+passw = raw_input("\033[39m[\033[31m*\033[39m] Diamond Free Fire Dalam Proses Tunggu 5 Menit buka FF")
